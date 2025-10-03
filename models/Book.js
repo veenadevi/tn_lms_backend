40325041667f529fcc9aff3f4b656bb0d757a816
VeenaQualitrix <veena.devi@qualitrix.com>
2025-10-04 00:07:02 +0530

    lms_backend

---
+++ models/Book.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const BookSchema = new mongoose.Schema({
+    bookName:{
+        type:String,
+        require:true
+    },
+    alternateTitle:{
+        type:String,
+        default:""
+    },
+    author:{
+        type:String,
+        require:true
+    },
+    language:{
+        type:String,
+        default:""
+    },
+    publisher:{
+        type:String,
+        default:""
+    },
+    bookCountAvailable:{
+        type:Number,
+        require:true
+    },
+    bookStatus:{
+        type:String,
+        default:"Available"
+    },
+    bookPrice:{
+        type: mongoose.Schema.Types.Decimal128,
+        default:100.00
+    },
+    categories:[{ 
+        type: mongoose.Types.ObjectId, 
+        ref: "BookCategory" 
+    }],
+    transactions:[{
+        type:mongoose.Types.ObjectId,
+        ref:"BookTransaction"
+    }]
+},
+{
+    timestamps:true
+})
+
+export default mongoose.model("Book",BookSchema)
